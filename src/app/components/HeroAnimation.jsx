dbaf301e95297463bdaff6568daaf726e14a3d72
jpntc <judepierre4477@gmail.com>
2024-05-26 14:21:46 -0400

    implemented better code

--- src/app/components/HeroAnimation.jsx
+++ src/app/components/HeroAnimation.jsx
@@ -8,26 +8,36 @@ const TechOrbs = [
     path: "/images/tech-stack-images/py.png",
     width: 100,
     height: 100,
+    styles:
+      "flex rounded-full  w-20 h-20 md:w-32 md:h-32 bg-gradient-to-r  from-blue-200 via-sky-400 to-yellow-400 p-4",
   },
   {
     path: "/images/tech-stack-images/js.png",
     width: 100,
     height: 100,
+    styles:
+      "flex rounded-full w-20 h-20 md:w-32 md:h-32 bg-gradient-to-r from-orange-300 to-white p-4 ",
   },
   {
     path: "/images/tech-stack-images/ts.png",
     width: 100,
     height: 100,
+    styles:
+      "flex rounded-full  w-20 h-20 md:w-32 md:h-32 bg-gradient-to-r from-blue-800 via-sky-600 to-white p-6  overflow-hidden ",
   },
   {
     path: "/images/tech-stack-images/jv.png",
     width: 100,
     height: 100,
+    styles:
+      "flex rounded-full  w-20 h-20 md:w-32 md:h-32 bg-gradient-to-r  from-red-500 to-blue-500 p-4 ",
   },
   {
     path: "/images/tech-stack-images/cpp.png",
     width: 100,
     height: 100,
+    styles:
+      "flex rounded-full  w-20 h-20 md:w-32 md:h-32 bg-gradient-to-r  from-blue-800  via-blue-500 to-white p-4 ",
   },
 ];
 
@@ -45,7 +55,7 @@ const HeroAnimation = () => {
                 key={index}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.8 }}
-                className="flex rounded-full  w-20 h-20 md:w-24 md:h-24 bg-gradient-to-r  from-blue-200 via-sky-400 to-yellow-400 p-4 "
+                className= {`${orb.styles}" pointer-events-none"`}
               >
                 <Image
                   src={orb.path}
