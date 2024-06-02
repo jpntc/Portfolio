36a93c2fa917541fb2780cba38f6d0907807f48a
jpntc <judepierre4477@gmail.com>
2024-06-01 23:05:32 -0400

    bug fixes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -16,7 +16,7 @@ const EmailSection = () => {
       message: e.target.message.value,
     };
     const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+    const endpoint = "./api/send";
 
     const options = {
       method: "POST",
