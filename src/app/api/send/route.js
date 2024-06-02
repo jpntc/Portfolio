6ecd97a487f6136569643baf0462431b99b59c51
jpntc <judepierre4477@gmail.com>
2024-06-02 19:09:22 -0400

    bug fixes

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,12 +1,13 @@
 import {Resend} from 'resend';
 import {NextResponse} from 'next/server';
 
-const API_KEY = process.env.RESEND_API_KEY;
-const resend = new Resend(API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-const MY_GMAIL = process.env.MY_GMAIL;
+
 
 export async function POST(req,res) {
+  const API_KEY = process.env.RESEND_API_KEY;
+  const resend = new Resend(API_KEY);
+  const fromEmail = process.env.FROM_EMAIL;
+  const MY_GMAIL = process.env.MY_GMAIL;
   const body =  req;
 
   const {email, subject, message} = body;
