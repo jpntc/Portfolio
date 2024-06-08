20e9bac681bc6152eb2c697f2ef27734dd8a6e7b
jpntc <judepierre4477@gmail.com>
2024-06-08 14:59:42 -0400

    testing fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,30 +1,37 @@
-import {Resend} from 'resend';
-import {NextResponse} from 'next/server';
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+import { m } from "framer-motion";
 
 const API_KEY = process.env.RESEND_API_KEY;
 const resend = new Resend(API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-const MY_GMAIL = process.env.MY_GMAIL;
+const from_email = process.env.FROM_EMAIL;
+const my_gmail = process.env.MY_GMAIL;
 
-export async function POST(req,res){
+export async function POST(req, res) {
   const body = await req.json();
-  const {email, subject, message} = body;
-  try {
-    const data = await resend.emails.send({ 
-      from: fromEmail,
-      to: [MY_GMAIL,email],
-      subject: subject,
-      react:(
-      <> 
-      <h1>{subject}</h1>
-        <p>{message}</p>
-        <p>Message submitted</p>
-        <p>Thank you for your inquiry!</p>
-        </>)
-    });
-    return  NextResponse.json(data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return  NextResponse.json(error);
+  console.log(from_email, my_gmail, API_KEY);
+  let data;
+  if (body) {
+    const { email, subject, message } = body;
+    console.log(email, subject, message);
+    try {
+      data = await resend.emails.send({
+        from: from_email,
+        to: [my_gmail, email],
+        subject: subject,
+        react: (
+          <>
+            <h1>{subject}</h1>
+            <p>{message}</p>
+            <p>Message submitted</p>
+            <p>Thank you for your inquiry!</p>
+          </>
+        ),
+      });
+    } catch (error) {
+      console.log("Error sending email:", error);
+      return NextResponse.json(error);
+    }
   }
+  return NextResponse.json(data);
 }
