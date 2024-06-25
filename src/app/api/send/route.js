a7246dfd5372a2881f22702bb5c37eae5b204c4e
jpntc <judepierre4477@gmail.com>
2024-06-25 18:03:13 -0400

    test for send api

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,30 +1,41 @@
-import {Resend} from 'resend';
-import {NextResponse} from 'next/server';
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
 
-const API_KEY = process.env.RESEND_API_KEY;
-const resend = new Resend(API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-const MY_GMAIL = process.env.MY_GMAIL;
+const initializeResend = async () => {
+  const API_KEY = process.env.RESEND_API_KEY;
+  const resend = new Resend(API_KEY);
+  const from_email = process.env.FROM_EMAIL;
+  const my_gmail = process.env.MY_GMAIL;
+  return [resend, from_email, my_gmail];
+};
 
-export async function POST(req,res){
+export async function POST(req, res) {
+  const [resend, from_email, my_gmail] = await initializeResend();
+  console.log(resend, from_email, my_gmail);
+
   const body = await req.json();
-  const {email, subject, message} = body;
-  try {
-    const data = await resend.emails.send({ 
-      from: fromEmail,
-      to: [MY_GMAIL,email],
-      subject: subject,
-      react:(
-      <> 
-      <h1>{subject}</h1>
-        <p>{message}</p>
-        <p>Message submitted</p>
-        <p>Thank you for your inquiry!</p>
-        </>)
-    });
-    return  NextResponse.json(data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return  NextResponse.json(error);
+  let data;
+  if (body) {
+    const { email, subject, message } = body;
+    console.log(email, subject, message);
+    try {
+      data = await resend.emails.send({
+        from: from_email,
+        to: [my_gmail, email],
+        subject: subject,
+        react: (
+          <>
+            <h1>{subject}</h1>
+            <p>{message}</p>
+            <p>Message submitted</p>
+            <p>Thank you for your inquiry!</p>
+          </>
+        ),
+      });
+    } catch (error) {
+      console.log("Error sending email:", error);
+      return NextResponse.json(error);
+    }
   }
+  return NextResponse.json(data);
 }
